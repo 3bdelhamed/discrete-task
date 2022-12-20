28be4c9fd5a98675db83d7e482eb60ae961e90d8
3bdelhamed <104280173+3bdelhamed@users.noreply.github.com>
2022-12-20 20:46:25 +0200

    Add files via upload

---
+++ Task/perfect number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PerfectNumberRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the lower bound of the range: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the upper bound of the range: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The perfect numbers in the range are: ");
+            for (int i = n1; i <= n2; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+                }
+
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Task/prime number/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https:using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the lower bound:");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the upper bound:");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Prime numbers between {n1} and {n2}:");
+            for (int i = n1; i <= n2; i++)
+            {
+                if (CheckPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static bool CheckPrime(int num)
+        {
+            if(num == 2) return true;
+            if(num <2 ||num%2==0) return false;
+
+            for (int i = 3; i <= Math.Sqrt(num); i+=2)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
